ed2089346c2b0182042753621bf467bb9f32b6c3
Alex Kocharin <alex@kocharin.ru>
2015-09-12 01:02:17 +0300

    Add a helper tool to check fixture files

---
+++ support/dump.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+'use strict';
+
+/* eslint-disable no-console */
+
+
+var files = process.argv.slice(2);
+
+if (!files.length) {
+  console.error('Usage: dump.js file1 [file2 [file3 ...]]');
+  return;
+}
+
+files.forEach(function (file) {
+  var input = require('fs').createReadStream(file);
+
+  input.on('error', function (err) {
+    console.log(file + ':', err);
+  });
+
+  require('../')(input, function (err, result) {
+    console.log(file + ':', err || result);
+    input.destroy();
+  });
+});
