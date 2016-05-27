1a3455915bf1b7deaf919ddecc796cdbf0dba00d
Vitaly Puzrin <vitaly@rcdesign.ru>
2016-05-28 02:35:31 +0300

    Stream: force parsers cleanup as soon as possible

--- stream.js
+++ stream.js
@@ -6,23 +6,50 @@ var error   = require('./lib/common').error;
 var parsers = require('./lib/parsers_stream');
 
 
+function unrecognizedFormat() {
+  return error('unrecognized file format', 'ECONTENT');
+}
+
+
 module.exports = function probeStream(stream, _callback) {
   var callback = once(_callback);
+  var pending  = 0;
+  var pStreams = [];
 
   // prevent "possible EventEmitter memory leak" warnings
   stream.setMaxListeners(0);
 
-  stream.on('error', function (err) { callback(err); });
+  function cleanup(strm) {
+    var i;
 
-  var pending = 0;
+    if (strm) {
+      i = pStreams.indexOf(strm);
+      if (i) {
+        stream.unpipe(pStreams[i]);
+        pStreams[i].end();
+        pStreams.splice(i, 1);
+      }
+      return;
+    }
+
+    for (i = 0; i < pStreams.length; i++) {
+      stream.unpipe(pStreams[i]);
+      pStreams[i].end();
+    }
+    pStreams.length = 0;
+  }
+
+  stream.on('error', function (err) { cleanup(); callback(err); });
+  stream.on('end',   function ()    { cleanup(); callback(unrecognizedFormat()); });
 
   Object.keys(parsers).forEach(function (type) {
-    var parser = parsers[type];
 
     pending++;
 
-    parser(stream, function (__, result) {
+    var pStream = parsers[type](stream, function (__, result) {
       pending--;
+
+      cleanup(pStream);
 
       if (result) {
         callback(null, result);
@@ -31,10 +58,13 @@ module.exports = function probeStream(stream, _callback) {
 
       // No more active scanners & still no positive -> fail
       if (!pending) {
-        callback(error('unrecognized file format', 'ECONTENT'));
+        cleanup();
+        callback(unrecognizedFormat());
         return;
       }
     });
+
+    pStreams.push(pStream);
   });
 };
 
