6e22cc3f3e315bcc166a23d29f8a1a799929f873
Oleg Vaskevich <oleg@osv.im>
2019-05-10 19:17:42 -0700

    Don't use any-promise

--- http.js
+++ http.js
@@ -17,14 +17,8 @@ var defaults = {
   }
 };
 
-var P;
-
-
 module.exports = function probeHttp(src, options) {
-  // lazy Promise init
-  P = P || require('any-promise');
-
-  return new P(function (resolve, reject) {
+  return new Promise(function (resolve, reject) {
     var req, length, finalUrl;
 
     try {
